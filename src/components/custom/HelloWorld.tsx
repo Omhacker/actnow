35652b881c882cf2ad4be48e567c245d07e9902c
Om Savani <omsavani207@gmail.com>
2025-03-30 10:40:38 +0530

    Setup folder structure and components

---
+++ src/components/custom/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelloWorld = () => {
+  return (
+    <div className="text-xl font-bold text-blue-500 p-4">
+      🚀 Hello, ACTNow!
+    </div>
+  );
+};
+
+export default HelloWorld;
